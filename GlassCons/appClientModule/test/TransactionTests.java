49ab77c99b7a36261a3f65617c4bd3688125df2a
Coronae <Arnold.Lhom@gmail.com>
2016-05-18 17:28:56 +0200

    Nettoyage + jUnit

---
+++ GlassCons/appClientModule/test/TransactionTests.java
@@ -0,0 +1,69 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import app.RestJson;
+import data.Transac;
+
+public class TransactionTests {
+
+	Transac transaction;
+
+	@Before
+	public void initialize() {
+		try {
+			transaction = RestJson.getListTransac().get(1);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void transactionIdNotNull() {
+		Assert.assertNotNull(transaction.getId());
+	}
+
+	@Test
+	public void transactionIdIsSet() {
+		Assert.assertTrue(transaction.getId() != 0);
+	}
+
+	@Test
+	public void transactionDateNotNull() {
+		Assert.assertNotNull(transaction.getDate());
+	}
+
+	@Test
+	public void typeNotNull() {
+		Assert.assertNotNull(transaction.getType());
+	}
+
+	@Test
+	public void typeIsSet() {
+		Assert.assertTrue(transaction.getType().length() != 0);
+	}
+
+	@Test
+	public void amountNotNull() {
+		Assert.assertNotNull(transaction.getAmount());
+	}
+
+	@Test
+	public void amountIsSet() {
+		Assert.assertTrue(transaction.getAmount() != 0);
+	}
+
+	@Test
+	public void customerIdNotNull() {
+		Assert.assertNotNull(transaction.getCustomerId());
+	}
+
+	@Test
+	public void customerIdIsSet() {
+		Assert.assertTrue(transaction.getCustomerId() != 0);
+	}
+
+}
